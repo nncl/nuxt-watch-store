13c1baa95119ab1f2b0b4520e9738f1d89ee8363
Cauê Almeida <nncl@live.com>
2022-04-28 20:20:04 -0300

    feat: Binding product properties

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,11 @@
       <div
         class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
       >
-        <product-card v-for="product in products" :key="product.id" />
+        <product-card
+          v-for="product in products"
+          :key="product.id"
+          :product="product"
+        />
       </div>
     </div>
   </main>
