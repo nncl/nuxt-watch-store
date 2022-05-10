d718f4e684268cf296518dee5c78d2669ee82b84
Cauê Almeida <nncl@live.com>
2022-05-10 20:18:36 -0300

    test(components): Refactoring creating product card

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -2,9 +2,22 @@ import { mount } from '@vue/test-utils';
 import ProductCard from '@/components/ProductCard';
 import { makeServer } from '@/miragejs/server';
 
-describe('ProductCard - unit', () => {
-  let server;
+let server;
 
+const mountProductCard = () => {
+  return mount(ProductCard, {
+    propsData: {
+      product: server.create('product', {
+        title: 'Pretty watch', // Overriding miragejs
+        price: '23.00', // Overriding miragejs
+        image:
+          'https://images.unsplash.com/photo-1495857000853-fe46c8aefc30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80', // Overriding miragejs
+      }),
+    },
+  });
+};
+
+describe('ProductCard - unit', () => {
   beforeEach(() => {
     server = makeServer({ environment: 'test' });
   });
@@ -14,32 +27,16 @@ describe('ProductCard - unit', () => {
   });
 
   it('should match snapshot', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Pretty watch', // Overriding miragejs
-          price: '23.00', // Overriding miragejs
-          image:
-            'https://images.unsplash.com/photo-1495857000853-fe46c8aefc30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80', // Overriding miragejs
-        }),
-      },
-    });
+    const wrapper = mountProductCard();
 
     expect(wrapper.element).toMatchSnapshot();
   });
 
   it('should mount the component', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Pretty watch', // Overriding miragejs
-          price: '22.00', // Overriding miragejs
-        }),
-      },
-    });
+    const wrapper = mountProductCard();
 
     expect(wrapper.vm).toBeDefined();
     expect(wrapper.text()).toContain('Pretty watch');
-    expect(wrapper.text()).toContain('$22.00');
+    expect(wrapper.text()).toContain('$23.00');
   });
 });
