94e53a2ac726d58beeda178603d7331d7212a394
Cauê Almeida <nncl@live.com>
2022-05-09 22:24:28 -0300

    test(components): Mocking element with mirageJS
    And also storing it as a snapshot as well.

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -1,14 +1,45 @@
 import { mount } from '@vue/test-utils';
 import ProductCard from '@/components/ProductCard';
+import { makeServer } from '@/miragejs/server';
 
 describe('ProductCard - unit', () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  it('should match snapshot', () => {
+    const wrapper = mount(ProductCard, {
+      propsData: {
+        product: server.create('product', {
+          title: 'Pretty watch', // Overriding miragejs
+          price: '23.00', // Overriding miragejs
+          image:
+            'https://images.unsplash.com/photo-1495857000853-fe46c8aefc30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80', // Overriding miragejs
+        }),
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
   it('should mount the component', () => {
     const wrapper = mount(ProductCard, {
       propsData: {
-        product: {},
+        product: server.create('product', {
+          title: 'Pretty watch', // Overriding miragejs
+          price: '22.00', // Overriding miragejs
+        }),
       },
     });
 
     expect(wrapper.vm).toBeDefined();
+    expect(wrapper.text()).toContain('Pretty watch');
+    expect(wrapper.text()).toContain('$22.00');
   });
 });
