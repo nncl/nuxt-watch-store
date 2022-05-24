39e45cd6e2ba6dcd28082da96ee0e257f0daee8a
Cauê Almeida <nncl@live.com>
2022-05-24 20:01:49 -0300

    feat(components): Removing promo code form

--- components/Cart.vue
+++ components/Cart.vue
@@ -25,20 +25,6 @@
     </div>
     <hr class="my-3" />
     <cart-item />
-    <div class="mt-8">
-      <form class="flex items-center justify-center">
-        <input
-          class="form-input w-48"
-          type="text"
-          placeholder="Add promocode"
-        />
-        <button
-          class="ml-3 flex items-center px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
-        >
-          <span>Apply</span>
-        </button>
-      </form>
-    </div>
     <a
       class="flex items-center justify-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
     >
