5d16f418e1f9b62c82e097128f231d2e0526ad25
Cauê Almeida <nncl@live.com>
2022-05-24 20:36:20 -0300

    feat(components): Toggling product quantity and its tests

---
+++ components/CartItem.unit.spec.js
@@ -0,0 +1,85 @@
+import { mount } from '@vue/test-utils';
+import CartItem from '@/components/CartItem';
+
+import { makeServer } from '@/miragejs/server';
+
+let server;
+
+const mountCartItem = () => {
+  const product = server.create('product', {
+    title: 'Cool watch',
+    price: '22.33',
+  });
+
+  const wrapper = mount(CartItem, {
+    propsData: {
+      product,
+    },
+  });
+  return { wrapper, product };
+};
+
+describe('CartItem - unit', () => {
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  it('should mount the component', () => {
+    const { wrapper } = mountCartItem();
+    expect(wrapper.vm).toBeDefined();
+  });
+
+  it('should display product info', () => {
+    const {
+      wrapper,
+      product: { title, price },
+    } = mountCartItem();
+    const content = wrapper.text();
+
+    expect(content).toContain(title);
+    expect(content).toContain(price);
+  });
+
+  it('should display 1 quantity when product is first shown', () => {
+    const { wrapper } = mountCartItem();
+    const quantity = wrapper.find('[data-testid="quantity"]');
+
+    expect(quantity.text()).toContain('1');
+  });
+
+  it('should increase quantity when + button is clicked', async () => {
+    const { wrapper } = mountCartItem();
+    const quantity = wrapper.find('[data-testid="quantity"]');
+    const button = wrapper.find('[data-testid="+"]');
+
+    await button.trigger('click');
+    expect(quantity.text()).toContain('2');
+    await button.trigger('click');
+    expect(quantity.text()).toContain('3');
+    await button.trigger('click');
+    expect(quantity.text()).toContain('4');
+  });
+
+  it('should decrease quantity when - button is clicked', async () => {
+    const { wrapper } = mountCartItem();
+    const quantity = wrapper.find('[data-testid="quantity"]');
+    const button = wrapper.find('[data-testid="-"]');
+
+    await button.trigger('click');
+    expect(quantity.text()).toContain('0');
+  });
+
+  it('should not below zero when button - is repeatedly clicked', async () => {
+    const { wrapper } = mountCartItem();
+    const quantity = wrapper.find('[data-testid="quantity"]');
+    const button = wrapper.find('[data-testid="-"]');
+
+    await button.trigger('click');
+    await button.trigger('click');
+    expect(quantity.text()).toContain('0');
+  });
+});
