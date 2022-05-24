c785290ee55b9f2b9ec4d892089bb03e5fecfb30
Cauê Almeida <nncl@live.com>
2022-05-23 23:42:07 -0300

    test(components): Testing cart toggling

---
+++ components/Cart.unit.spec.js
@@ -0,0 +1,35 @@
+import { mount } from '@vue/test-utils';
+import Cart from '@/components/Cart';
+
+describe('Cart - unit', () => {
+  it('should mount the component', () => {
+    const wrapper = mount(Cart);
+    expect(wrapper.vm).toBeDefined();
+  });
+
+  it('should emit close event', async () => {
+    const wrapper = mount(Cart);
+    const button = wrapper.find('[data-testid="close-button"]');
+
+    await button.trigger('click');
+
+    expect(wrapper.emitted().close).toBeTruthy();
+    expect(wrapper.emitted().close).toHaveLength(1);
+  });
+
+  it('should hide the cart when no prop isOpen is passed', () => {
+    const wrapper = mount(Cart);
+
+    expect(wrapper.classes()).toContain('hidden');
+  });
+
+  it('should display the cart when no prop isOpen is passed', () => {
+    const wrapper = mount(Cart, {
+      propsData: {
+        isOpen: true,
+      },
+    });
+
+    expect(wrapper.classes()).not.toContain('hidden');
+  });
+});
