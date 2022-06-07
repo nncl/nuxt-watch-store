ce4df228254e8e2b8bb7153092c036729e0265a9
Cauê Almeida <nncl@live.com>
2022-06-06 22:08:15 -0300

    test(e2e): Setting up first e2e tests

---
+++ cypress/e2e/Store.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+context('Store', () => {
+  it('should display the store', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.get('body').contains('Brand');
+    cy.get('body').contains('Wrist Watch');
+  });
+});
