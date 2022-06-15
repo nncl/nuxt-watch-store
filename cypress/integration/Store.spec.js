2e5d5d0523ad5fbbdfd3357573bb142fa71d7f7d
Cauê Almeida <nncl@live.com>
2022-06-14 21:42:32 -0300

    test(e2e): Including commands

--- cypress/integration/Store.spec.js
+++ cypress/integration/Store.spec.js
@@ -2,6 +2,8 @@ import { makeServer } from '../../miragejs/server';
 
 context('Store', () => {
   let server;
+  const g = cy.get;
+  const gid = cy.getByTestId;
 
   beforeEach(() => {
     server = makeServer({ environment: 'test' });
@@ -14,50 +16,47 @@ context('Store', () => {
   it('should display the store', () => {
     cy.visit('/');
 
-    cy.get('body').contains('Brand');
-    cy.get('body').contains('Wrist Watch');
+    g('body').contains('Brand');
+    g('body').contains('Wrist Watch');
   });
 
   context('Store > Shopping Cart', () => {
     it('should not display shopping cart when page first loads', () => {
       cy.visit('/');
-      cy.get('[data-testid="shopping-cart"]').should('have.class', 'hidden');
+      gid('shopping-cart').should('have.class', 'hidden');
     });
 
     it('should toggle shopping cart visibility cart when button is clicked', () => {
       cy.visit('/');
-      cy.get('[data-testid="toggle-cart"]').as('toggleButton');
-      cy.get('@toggleButton').click();
-      cy.get('[data-testid="shopping-cart"]').should(
-        'not.have.class',
-        'hidden'
-      );
-      cy.get('@toggleButton').click({ force: true });
-      cy.get('[data-testid="shopping-cart"]').should('have.class', 'hidden');
+      gid('toggle-cart').as('toggleButton');
+      g('@toggleButton').click();
+      gid('shopping-cart').should('not.have.class', 'hidden');
+      g('@toggleButton').click({ force: true });
+      gid('shopping-cart').should('have.class', 'hidden');
     });
   });
 
   context('Store  > Product List', () => {
     it('should display "0 Products" when no product is returned', () => {
       cy.visit('/');
-      cy.get('[data-testid="card-list"]').should('have.length', 0);
-      cy.get('body').contains('0 Products');
+      gid('card-list').should('have.length', 0);
+      g('body').contains('0 Products');
     });
 
     it('should display "1 Product" when 1 product is returned', () => {
       server.create('product');
 
       cy.visit('/');
-      cy.get('[data-testid="card-list"]').should('have.length', 1);
-      cy.get('body').contains('1 Product');
+      gid('card-list').should('have.length', 1);
+      g('body').contains('1 Product');
     });
 
     it('should display "10 Products" when 10 products are returned', () => {
       server.createList('product', 10);
 
       cy.visit('/');
-      cy.get('[data-testid="card-list"]').should('have.length', 10);
-      cy.get('body').contains('10 Products');
+      gid('card-list').should('have.length', 10);
+      g('body').contains('10 Products');
     });
   });
 
@@ -65,7 +64,7 @@ context('Store', () => {
     it('should type in the search field', () => {
       cy.visit('/');
 
-      cy.get('input[type="search"]')
+      g('input[type="search"]')
         .type('Hello world')
         .should('have.value', 'Hello world');
     });
@@ -77,19 +76,19 @@ context('Store', () => {
       server.createList('product', 10);
 
       cy.visit('/');
-      cy.get('input[type="search"]').type('Awesome watch');
-      cy.get('[data-testid="search-form"]').submit();
-      cy.get('[data-testid="card-list"]').should('have.length', 1);
+      g('input[type="search"]').type('Awesome watch');
+      gid('search-form').submit();
+      gid('card-list').should('have.length', 1);
     });
 
     it('should not return any product', () => {
       server.createList('product', 10);
 
       cy.visit('/');
-      cy.get('input[type="search"]').type('Awesome watch');
-      cy.get('[data-testid="search-form"]').submit();
-      cy.get('[data-testid="card-list"]').should('have.length', 0);
-      cy.get('body').contains('0 Products');
+      g('input[type="search"]').type('Awesome watch');
+      gid('search-form').submit();
+      gid('card-list').should('have.length', 0);
+      g('body').contains('0 Products');
     });
   });
 });
