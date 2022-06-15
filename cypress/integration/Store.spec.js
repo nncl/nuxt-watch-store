51c634491443d23a9ba1efe485a7be7e3c1d6605
Cauê Almeida <nncl@live.com>
2022-06-14 21:34:59 -0300

    test(e2e): Testing cart button toggling

--- cypress/integration/Store.spec.js
+++ cypress/integration/Store.spec.js
@@ -18,6 +18,25 @@ context('Store', () => {
     cy.get('body').contains('Wrist Watch');
   });
 
+  context('Store > Shopping Cart', () => {
+    it('should not display shopping cart when page first loads', () => {
+      cy.visit('/');
+      cy.get('[data-testid="shopping-cart"]').should('have.class', 'hidden');
+    });
+
+    it('should toggle shopping cart visibility cart when button is clicked', () => {
+      cy.visit('/');
+      cy.get('[data-testid="toggle-cart"]').as('toggleButton');
+      cy.get('@toggleButton').click();
+      cy.get('[data-testid="shopping-cart"]').should(
+        'not.have.class',
+        'hidden'
+      );
+      cy.get('@toggleButton').click({ force: true });
+      cy.get('[data-testid="shopping-cart"]').should('have.class', 'hidden');
+    });
+  });
+
   context('Store  > Product List', () => {
     it('should display "0 Products" when no product is returned', () => {
       cy.visit('/');
