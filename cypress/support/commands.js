3dc30681f9e655fab5f5ecfb46a319a02820c3c6
Cauê Almeida <nncl@live.com>
2022-06-16 23:29:49 -0300

    refactor(tests): Re-usable functions when testing cart

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,43 @@
 Cypress.Commands.add('getByTestId', (selector) => {
   cy.get(`[data-testid="${selector}"]`);
 });
+
+Cypress.Commands.add('addToCart', (mode) => {
+  cy.getByTestId('card-list').as('productCards');
+
+  const addByIndex = () => {
+    click(mode);
+  };
+
+  const click = (index) =>
+    cy.get('@productCards').eq(index).find('button').click({ force: true });
+
+  const addByIndexes = () => {
+    for (const index of mode) {
+      click(index);
+    }
+  };
+
+  const addAll = () => {
+    cy.get('@productCards').then(($elements) => {
+      let i = 0;
+
+      while (i < $elements.length) {
+        click(i);
+        i++;
+      }
+    });
+  };
+
+  if (Array.isArray(mode)) {
+    addByIndexes();
+  } else if (typeof mode === 'number') {
+    addByIndex();
+  } else if (typeof mode === 'string' && mode === 'all') {
+    addAll();
+  } else {
+    throw new Error(
+      'Please provide a valid input for cy.addToCart().\r\nPossible values are array, number and "all"'
+    );
+  }
+});
