81be731beede1ffefcfe0aae5161e1a54c8e6881
Cauê Almeida <nncl@live.com>
2022-06-21 22:12:56 -0300

    test(e2e): Testing product cart quantities

--- cypress/integration/Store.spec.js
+++ cypress/integration/Store.spec.js
@@ -77,6 +77,38 @@ context('Store', () => {
       gid('cart-list').should('have.length', quantity);
     });
 
+    it('should display quantity 1 when product is added to cart', () => {
+      cy.addToCart({ index: 1 });
+      gid('quantity').contains(1);
+    });
+
+    it('should increase quantity when button + gets clicked', () => {
+      cy.addToCart({ index: 1 });
+      gid('+').click();
+      gid('quantity').contains(2);
+      gid('+').click();
+      gid('quantity').contains(3);
+    });
+
+    it('should decrease quantity when button - gets clicked', () => {
+      cy.addToCart({ index: 1 });
+      gid('+').click();
+      gid('+').click();
+      gid('quantity').contains(3);
+      gid('-').click();
+      gid('quantity').contains(2);
+      gid('-').click();
+      gid('quantity').contains(1);
+    });
+
+    it('should NOT decrease below zero when button - gets clicked', () => {
+      cy.addToCart({ index: 1 });
+      gid('-').click();
+      gid('-').click();
+      gid('-').click();
+      gid('quantity').contains(0);
+    });
+
     it('should remove a product from the cart', () => {
       cy.addToCart({ index: 2 });
 
